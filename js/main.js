29563e65d98fcdf2809ecf091ff7e32e1a4459ce
bluewarrior-rpiab <bluewarrior.slayersonline@gmail.com>
2024-08-04 22:07:09 +0200

    migrating js code to a single file

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import {Chessground} from './chessground.min.js';
+const board = Chessground(document.getElementById('board-1'), {});
+const chessWorker = new Worker('./scalachess.min.js');
+chessWorker.onmessage = function(event){
+	console.log(event.data);
+};
+
+function openTab(tabName) {
+	var i;
+	var x = document.getElementsByClassName("tab");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";  
+	}
+	document.getElementById(tabName).style.display = "block";  
+}			
