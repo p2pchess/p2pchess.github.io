48484a5739ccbf2f38a854bf5e1046cf68382d8f
bluewarrior-rpiab <bluewarrior.slayersonline@gmail.com>
2024-08-04 22:10:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 import {Chessground} from './chessground.min.js';
+
 const board = Chessground(document.getElementById('board-1'), {});
 const chessWorker = new Worker('./scalachess.min.js');
 chessWorker.onmessage = function(event){
 	console.log(event.data);
 };
 
-function openTab(tabName) {
+export function openTab(tabName) {
 	var i;
 	var x = document.getElementsByClassName("tab");
 	for (i = 0; i < x.length; i++) {
