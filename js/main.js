9082965b963aa27f73d68d9b2e86fec3313d5b60
bluewarrior-rpiab <giovanni.falletta@gmail.com>
2024-08-06 00:12:59 +0200

    testing peer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-// import the chess board object
+// import the chess board object and the peer object
 import {Chessground} from './chessground.min.js';
+import {Peer} from 'https://esm.sh/peerjs@1.5.4?bundle-deps';
 
 // globals :)
 const	tabs		= document.getElementsByClassName("tab"),
@@ -17,16 +18,30 @@ function openTab(evt, tabName) {
 	evt.currentTarget.className += " tab-item-select";
 }			
 
+// Entry point
 (function() {
-	// test board
-	//Chessground(document.getElementById('chessground'), {});
 	// run the chess worker
 	const chessWorker = new Worker('./scalachess.min.js');
 	chessWorker.onmessage = function(event){
 		console.log(event.data);
 	};
 
+	// connect to peer network
+	var userId = 1;
+	var peer = new Peer('p2p-user' + userId);
+	peer.on('open', function(id) {
+		console.log('My peer ID is: ' + id);
+	});
+	peer.on('error', function(err) {
+		if (err.type == 'unavailable-id') {
+			console.log('User id unavailable, trying next');
+			userId++;
+			peer = new Peer('p2p-user' + userId);
+		}
+	});
 	// some functions need to be accessed via html page
 	window.openTab = openTab;
 })();
 
+	// test board
+	//Chessground(document.getElementById('chessground'), {});
