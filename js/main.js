04186a959824d7586310db9a7df42396cbf8c696
bluewarrior-rpiab <giovanni.falletta@gmail.com>
2024-08-05 15:14:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
-import {Chessground} from './chessground.min.js';
-
-//const board = Chessground(document.getElementById('chessground'), {});
-const chessWorker = new Worker('./scalachess.min.js');
-chessWorker.onmessage = function(event){
-	console.log(event.data);
-};
+// globals :)
+const	tabs		= document.getElementsByClassName("tab"),
+		tablinks	= document.getElementsByClassName("tab-links");
 
 function openTab(evt, tabName) {
 	var i;
-	var x = document.getElementsByClassName("tab");
-	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";  
+	for (i = 0; i < tabs.length; i++) {
+		tabs[i].style.display = "none";  
 	}
-	var tablinks = document.getElementsByClassName("tab-item");
-	for (i = 0; i < x.length; i++) {
+	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace("tab-item-select", "");
 	}
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " tab-item-select";
 }			
 
-window.openTab = openTab;
+(function() {
+	// import the chess board object
+	import {Chessground} from './chessground.min.js';
+
+	// run the chess worker
+	const chessWorker = new Worker('./scalachess.min.js');
+	chessWorker.onmessage = function(event){
+		console.log(event.data);
+	};
+
+	// some functions need to be accessed via html page
+	window.openTab = openTab;
+})();
+
