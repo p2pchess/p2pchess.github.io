7533678be48a632e8f1544b203cdde6f051b9736
bluewarrior-rpiab <giovanni.falletta@gmail.com>
2024-08-06 22:51:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ function connectPeer()
 	});
 	peer.on('open', function(id) {
 		accountName.innerText = usernameField.value = userName = id;
-		userNameField.addEventListener('change', function(){
+		usernameField.addEventListener('change', function(){
 			accountName.innerText = userName = this.value;
 		});
 		// Listening for the connection of the next user
