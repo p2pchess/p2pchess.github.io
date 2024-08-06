7acb931e50f2be9c1eb426173773ab7b7c60247b
bluewarrior-rpiab <giovanni.falletta@gmail.com>
2024-08-06 04:46:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ import {Peer} from 'https://esm.sh/peerjs@1.5.4?bundle-deps';
 const 	tabs = document.getElementsByClassName("tab"),
 		tablinks = document.getElementsByClassName("tab-item");
 
-var	peer = null, userId = 1;
+var	peer = null, previousPeer = null, nextPeer = null, userId = 1;
+
+// UI Functions
 
 function openTab(buttonId, tabName) {
 	var i;
@@ -20,23 +22,49 @@ function openTab(buttonId, tabName) {
 	document.getElementById(buttonId).className += " tab-item-select";
 }			
 
+// Network functions
+
 function connectPeer()
 {
 	peer = new Peer('p2pchess-user' + userId);
 
 	peer.on('error', function(err) {
 		if (err.type == 'unavailable-id') {
-			console.log('User id unavailable, trying next');
 			userId++;
 			connectPeer(userId);
+		}
+		else if (err.type == 'peer-unavailable') {
+			console.log('coucou');
 		}
 	});
 	peer.on('open', function(id) {
 		document.getElementById('account-name').innerText = 'p2pchess-user' + userId;
+		// Listening for the next connection
+		peer.on('connection', function(dataConnection) {
+			console.log('connection du pti nouveau');
+			nextPeer = dataConnexion;
+		});
+		// If not the number 1 user,try to connect to the previous peer with the closest id number
+		if (userId > 1)
+			findPreviousPeer();
 	});
 }
 
+function findPreviousPeer()
+{
+	/*var i, peerFound = false;
+	for (i = userId - 1; i > 0 && peerFound == false; i--)
+	{
+		previousPeer = peer.connect('p2pchess-user' + i);
+		previousPeer
+	}*/
+	console.log(previousPeer);
+	previousPeer = peer.connect('lacacaddldld');
+	console.log(previousPeer);
+}
+
 // Entry point
+
 (function() {
 	// run the chess worker
 	const chessWorker = new Worker('./scalachess.min.js');
@@ -47,7 +75,7 @@ function connectPeer()
 	// connect to peer network
 	connectPeer();
 
-	// some functions need to be accessed via html page
+	//adding ui functions in the window object
 	window.openTab = openTab;
 })();
 
